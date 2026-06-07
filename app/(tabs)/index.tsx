de841ddd5888c1eca86a3244a0a04b7dad3f5c25
n4r3xDev <134960234+n4r3xDev@users.noreply.github.com>
2026-06-07 18:35:26 +0200

    Home screen layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,17 @@ import {useState} from 'react';
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const [locationText, setLocationText] = useState("Brak zapisanej lokalizacji");
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <Pressable style={styles.pressable} onPress={() => alert("Lokalizacja zapisana!")}>
-          <Text style={styles.text}>Zapisz lokalizacje</Text>
+        <Text style={styles.textHeader}>Gdzie zaparkowałem?</Text>
+        <Text style={styles.text}>Zapisz lokalizację auta</Text>
+        <Pressable style={styles.pressable} onPress={() => setLocationText("Pozycja auta została zapisana!")}>
+          <Text style={styles.textButton}>Zapisz pozycję</Text>
         </Pressable>
+        <Text style={styles.textLocation}>{locationText}</Text>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -26,9 +31,24 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     borderRadius: 8,
   },
-  text: {
+  textButton: {
     color: "#FFFFFF",
     fontSize: 16,
     fontWeight: "bold",
-  }
+  },
+  textHeader: {
+    fontSize: 24,
+    marginBottom: 10,
+    color: "#333333",
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 20,
+    color: "#666666",
+  },
+  textLocation: {
+    fontSize: 16,
+    marginTop: 20,
+    color: "#333333",
+  },
 });
